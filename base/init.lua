10ed9366c4ac13c55381582260a9b9941bda5c4c
lomarco <ororr069@gmail.com>
2025-09-14 12:24:58 +0700

    Rewrite rename func

--- base/init.lua
+++ base/init.lua
@@ -159,13 +159,13 @@ require('lazy').setup({
         vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
         vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
         vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+        vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, opts)
 
         vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
         vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
         vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
 
         vim.keymap.set('n', '<leader>f', ':lua vim.lsp.buf.format()<CR>', { noremap = true, silent = true })
-        vim.keymap.set('n', '<A-r>', ':lua vim.lsp.buf.rename()<CR>', { noremap = true, silent = true })
       end
 
       local servers = { 'clangd', 'pyright', 'lua_ls', 'rust_analyzer' }
