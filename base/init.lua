9a3ca82a2f52c557eab7b7cb594cd968c50f0df0
lomarco <ororr069@gmail.com>
2025-10-02 20:29:55 +0700

    Delete compiler plugins

--- base/init.lua
+++ base/init.lua
@@ -295,23 +295,25 @@ require('lazy').setup({
     end
   },
   {
-    "Zeioth/compiler.nvim",
-    cmd = { "CompilerOpen", "CompilerToggleResults", "CompilerRedo" },
-    dependencies = { "stevearc/overseer.nvim", "nvim-telescope/telescope.nvim" },
-    opts = {},
+    'nvim-mini/mini.tabline',
+    version = '*',
+    opts = {
+      show_icons = false,
+      format = nil,
+      tabpage_section = 'left'
+    }
   },
   {
-    "stevearc/overseer.nvim",
-    commit = "6271cab7ccc4ca840faa93f54440ffae3a3918bd",
-    cmd = { "CompilerOpen", "CompilerToggleResults", "CompilerRedo" },
-    opts = {
-      task_list = {
-        direction = "bottom",
-        min_height = 25,
-        max_height = 25,
-        default_detail = 1
-      },
-    },
+    'm4xshen/autoclose.nvim',
+    config = function()
+      require('autoclose').setup()
+    end
+  },
+  {
+    "preservim/tagbar",
+    config = function()
+      vim.g.tagbar_autofocus = 1
+    end,
   },
   {
     "stevearc/oil.nvim",
