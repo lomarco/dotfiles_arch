19e4c390b7157847c57ba6748c8e43cfd347e4e6
lomarco <ororr069@gmail.com>
2025-10-18 01:54:07 +0700

    Add gd with telescope

--- base/init.lua
+++ base/init.lua
@@ -153,9 +153,11 @@ require('lazy').setup({
     config = function()
       local on_attach = function(_, bufnr)
         local opts = { noremap = true, silent = true, buffer = bufnr }
-        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+        -- vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
         vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
         vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+        vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, opts)
+        vim.keymap.set('n', 'gd', require('telescope.builtin').lsp_definitions, opts)
         vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
         vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, opts)
 
