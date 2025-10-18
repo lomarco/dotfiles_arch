2cc336d0d4d25dd06766210187a7cdeb0933b6b8
lomarco <ororr069@gmail.com>
2025-10-18 13:00:12 +0700

    Fix gr keymap

--- base/init.lua
+++ base/init.lua
@@ -153,7 +153,7 @@ require('lazy').setup({
       local on_attach = function(_, bufnr)
         local opts = { noremap = true, silent = true, buffer = bufnr }
         vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-        vim.keymap.set('n', 'gr', vim.lsp_buf.lsp_references, opts)
+        vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
         vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
         vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, opts)
         vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
