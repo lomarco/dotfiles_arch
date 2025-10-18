3054647899e5cb2337f6bec02711397d8a048794
lomarco <ororr069@gmail.com>
2025-10-18 15:59:03 +0700

    Delete mappings for cmp

--- base/init.lua
+++ base/init.lua
@@ -200,24 +200,7 @@ require('lazy').setup({
           { name = 'buffer',   priority = 500 },
           { name = 'path',     priority = 250 },
         },
-        mapping = cmp.mapping.preset.insert({
-          ['<C-Space>'] = cmp.mapping.complete(),
-          ['<CR>'] = cmp.mapping.confirm({ select = true }),
-          ['<Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            else
-              fallback()
-            end
-          end, { 'i', 's' }),
-          ['<S-Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            else
-              fallback()
-            end
-          end, { 'i', 's' }),
-        }),
+        mapping = cmp.mapping.preset.insert()
       }
     end,
   },
