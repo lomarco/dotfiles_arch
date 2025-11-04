18db4297716956aa7288d5d02ed46234940b1e69
lomarco <ororr069@gmail.com>
2025-11-04 11:27:53 +0700

    Delete marckdown prev

--- base/init.lua
+++ base/init.lua
@@ -424,10 +424,4 @@ require('lazy').setup({
       })
     end,
   },
-  {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = { "markdown" },
-    build = function() vim.fn["mkdp#util#install"]() end,
-  }
 })
